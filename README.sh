f5b1468bdaa3a915da93db9129fff19cb8340e63
vedantthete <vedantthete@gmail.com>
2022-11-12 06:44:26 +0530

    Update README.sh

--- README.sh
+++ README.sh
@@ -1,39 +1,25 @@
-k8s-install
-################################## EVERY NODE ##########################################
-
+############################################ ALL NODES ########################################
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
-sudo apt-get update &amp;&amp; sudo apt-get install -y apt-transport-https curl
-
-Add the google cloud package repository to your sources
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+# Add the google cloud package repository to your sources
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-cat &lt;&lt;EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 sudo apt-get update
 
-Install components
+# Install components
 sudo apt-get install -y kubelet kubeadm kubectl
-
-Hold version
+# Hold version
 sudo apt-mark hold kubelet kubeadm kubectl
 sudo rm /etc/containerd/config.toml
 sudo systemctl restart containerd
 
-########################################################################################
-
-################################## MASTER NODE #########################################
+############################################ MASTER NODES ########################################
 
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=0.0.0.0
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+sudo chown (id -g) $HOME/.kube/config
 kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
-
-########################################################################################
-
-################################## WORKER NODE #########################################
-
-sudo kubeadm join 10.0.0.134:6443 --token xxxx --discovery-token-ca-cert-hash xxxx
-
-########################################################################################
