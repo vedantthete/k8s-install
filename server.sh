1a8cf6bad2b00bfb6f1622a63fd12ea51f4dd028
vedantthete <vedantthete@gmail.com>
2022-11-19 20:52:00 +0530

    Create server.sh

---
+++ server.sh
@@ -0,0 +1,37 @@
+sudo iptables -F
+sudo iptables -X
+sudo iptables -t nat -F
+sudo iptables -t nat -X
+sudo iptables -t mangle -F
+sudo iptables -t mangle -X
+sudo iptables -t raw -F
+sudo iptables -t raw -X
+sudo iptables -t security -F
+sudo iptables -t security -X
+sudo apt-get update -y
+
+sudo apt-get install docker.io -y
+
+sudo systemctl enable docker
+
+sudo systemctl start docker
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+
+sudo apt-get install curl -y
+
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+
+sudo apt-get install kubeadm kubelet kubectl -y
+
+sudo apt-mark hold kubeadm kubelet kubectl
+
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl taint node m1 node-role.kubernetes.io/master:NoSchedule-
+
+sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
