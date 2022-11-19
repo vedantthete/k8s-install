e08eba8912d3ebdcaddd0c96f42632f325d19480
vedantthete <vedantthete@gmail.com>
2022-11-19 20:14:03 +0530

    Update README.sh

--- README.sh
+++ README.sh
@@ -25,3 +25,18 @@ sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Do
 #============================================================== V command only on workers ============================================================
 
 kubeadm join 10.0.0.40:6443 --token abc --discovery-token-ca-cert-hash sha256:def
+
+
+#=============================================================== Reinstall ===========================================================================
+sudo kubeadm reset
+sudo iptables -F
+sudo iptables -X
+sudo iptables -t nat -F
+sudo iptables -t nat -X
+sudo iptables -t mangle -F
+sudo iptables -t mangle -X
+sudo iptables -t raw -F
+sudo iptables -t raw -X
+sudo iptables -t security -F
+sudo iptables -t security -X
+rm -rf $HOME/.kube/config
